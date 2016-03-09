01ed975219952aa7056079f64c311051acc8293c
PSL <pramodsankar.l@gmail.com>
2016-03-08 21:54:31 -0500

    Modularize with init config

--- gruntFile.js
+++ gruntFile.js
@@ -1,32 +1,5 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        serve: {
-            options: {
-                port: 9000
-            }
-        },
-        ts: {
-            options: {
-                compiler: './node_modules/typescript/bin/tsc'
-            },
-            default: {
-                tsconfig: {
-                    passThrough: true
-                },
-                watch: '.'
-            }
-        },
-        karma: {
-            unit: {
-                configFile: './karma.conf.js'
-            }
-        }
-    });
-
-    grunt.loadNpmTasks('grunt-serve');
-    grunt.loadNpmTasks('grunt-ts');
-    grunt.loadNpmTasks('grunt-karma');
-
+    require('load-grunt-config')(grunt);
     grunt.registerTask('compile', ['ts']);
     grunt.registerTask('test', ['karma:unit']);
     grunt.registerTask('default', ['serve']);
